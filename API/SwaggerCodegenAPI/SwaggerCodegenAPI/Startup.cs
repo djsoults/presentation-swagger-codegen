ba7a7b2acc2c041e30e59b0dcb138c7ed0434897
DJ Soults <dj@illuminateddev.com>
2018-10-30 22:12:57 -0500

    Install and set up swashbuckle

--- API/SwaggerCodegenAPI/SwaggerCodegenAPI/Startup.cs
+++ API/SwaggerCodegenAPI/SwaggerCodegenAPI/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace SwaggerCodegenAPI
 {
@@ -27,6 +28,12 @@ namespace SwaggerCodegenAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "My API", Version = "v1" });
+            });
 
             services.AddSingleton<IFixture, Fixture>();
         }
@@ -44,6 +51,17 @@ namespace SwaggerCodegenAPI
             }
 
             app.UseHttpsRedirection();
+            
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+            
             app.UseMvc();
         }
     }
